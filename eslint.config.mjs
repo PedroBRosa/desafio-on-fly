37ae72a8d66feb7ab7c1fd6fac7eb815fe9f0538
Pedro Rosa <pedro.brosa01@gmail.com>
2025-11-13 21:22:16 -0300

    ci: adcionando arquivos de configuração do projeto

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+import playwright from 'eslint-plugin-playwright'
+
+export default [
+  // Configuração do ESLint + TypeScript
+  ...tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+  ),
+
+  // Configuração do Prettier
+  {
+    name: 'prettier',
+    plugins: { prettier },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+
+  // Configuração do Playwright para testes
+  {
+    name: 'playwright',
+    files: ['tests/**'],
+    ...playwright.configs['flat/recommended'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/expect-expect': 'off',
+      'playwright/no-force-option': 'off',
+      'playwright/no-wait-for-timeout': 'off',
+      'playwright/no-conditional-in-test': 'off',
+    },
+  },
+]
