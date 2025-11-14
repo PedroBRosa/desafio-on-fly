283a27bde3510f1df4be1e7dd6f51bf404ffe63e
Pedro Rosa <pedro.brosa01@gmail.com>
2025-11-14 15:13:01 -0300

    ci: mudando a plataforma para realizar os testes de compra

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASE_URL || 'https://demo.nopcommerce.com',
+    baseURL: process.env.BASE_URL || 'https://www.saucedemo.com/',
 
+    screenshot: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
